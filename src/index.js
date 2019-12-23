4b1f8f6bb3fb148dc0c25ba9bae2fe19d0e5c21c
yashpandit <yashpandit1995@gmail.com>
2019-12-23 17:04:21 +0530

    Add App.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
-
-const App = () => {
-  return <h1>Portfolio</h1>;
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
